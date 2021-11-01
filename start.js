74d581831516912aca7daabcfebe672000ecfa6a
matissz <matiss.zilgalvis@circlekeurope.com>
2021-11-01 17:39:45 +0200

    Seperating js from main index file

---
+++ start.js
@@ -0,0 +1,14 @@
+let canvas = document.querySelector('canvas');
+            let context = canvas.getContext('2d');
+
+            context.fillStyle = 'blue';
+            // context.fillRect(100, 100, 400, 400);
+
+            context.lineWidth = 4;
+            context.beginPath();
+            context.rect(100, 100, 400, 400);
+            context.stroke();
+            
+            context.beginPath();
+            context.arc(300, 300, 100, 0, Math.PI*2);
+            context.stroke();
